609be32022f118d7f8acd8aee760a21a9013d8b4
kdiphone <kdiphone24@gmail.com>
2021-05-26 09:12:13 +0000

    added northwind services

--- webapp/controller/rootView.controller.js
+++ webapp/controller/rootView.controller.js
@@ -6,6 +6,42 @@ sap.ui.define([
 	return Controller.extend("IntQ.IQuestion.controller.rootView", {
 		onInit: function () {
 
+		},
+		onBeforeRendering : function(){
+			var oJsonModel = new sap.ui.model.json.JSONModel();                  
+			var oDataModel = this.getView().getModel();
+			var oList = this.getView().byId("table1");
+			
+			var that = this;
+			oDataModel.read("/Customers", {
+				"method":"GET",
+				"success":function(data){
+					console.log(data);
+					oJsonModel.setData(data);
+					oList.setModel(oJsonModel,"JSONMODEL");
+				//	that.getView().byId("header0").setTitle = oJsonModel.getData().results[0].CompanyName;      
+				//	that.getView().byId("header0").setTitle ="KKDKKDKDKDDKDKDK";
+					//this.getView().byId("movie_name");
+
+				}
+				
+			});
 		}
 	});
 });
+
+/*
+	var oList = this.getView().byId("table1");
+			var oJsonModel = new sap.ui.model.json.JSONModel();
+			// oList.setModel(oModel);
+
+			var oModel = this.getView().getModel("ES5Model");
+			oModel.read("/Suppliers", {
+				"method": "GET",
+				"success": function (data) {
+					console.log(data);
+					oJsonModel.setData(data);
+					oList.setModel(oJsonModel, "JSONMODEL");
+				}
+			});
+*/
